2af6255cc2da114a21304dfdee60a1f0825591d9
Арсений Сендецкий <ars_121229@mail.ru>
2021-10-13 16:34:13 +0800

    Полностью сделал рассчет по кнопке

--- Form1.cs
+++ Form1.cs
@@ -15,12 +15,12 @@ namespace guif2
         public Form1()
         {
             InitializeComponent();
-            //SumA.Text = Properties.Settings.Default.SumForA; сохранение полей
+            SumA.Text = Properties.Settings.Default.SumA;
         }
 
         public class Logic
             {
-            public static int firstcond(double A, double B)
+            public static string firstcond(double A, double B)
             {
                 int month1 = 0;
                 double A1 = A;
@@ -33,10 +33,11 @@ namespace guif2
                     else if (A1 - A >= B) i = 1000;
                     if (A2 > B) i = 1000;
                 }
-                Console.WriteLine("\nЗа {0}й мес. величина ежемесячного увеличения вклада превысит {1} руб", month1, B);
-                return month1;
+                string result1 = "За "+month1+"й мес. величина ежемесячного увеличения вклада превысит "+B+" руб";
+                //MessageBox.Show(result1);
+                return result1;
             }
-            public static int secondcond(double A, double C)
+            public static string secondcond(double A, double C)
             {
                 double A2 = A;
                 int month2 = 0;
@@ -48,54 +49,32 @@ namespace guif2
                         month2 = month2 + 1;
                     }
                 }
-                Console.WriteLine("\nЧерез {0} мес. размер вклада превысит {1} руб", month2, C);
-                return month2;
+                string result2 = "Через " + month2 + " мес. размер вклада превысит " + C + " руб";
+               // MessageBox.Show(result2);
+                return result2;
             }
             public static int correctinput(double A, double B, double C)
             {
                 int a = 0;
-                if (A <= 0 || B <= 0 || C <= 0)
-                {
-                    Console.WriteLine("Неверные исходные данные! Закрытие программы...");
-                }
+                if (A <= 0 || B <= 0 || C <= 0) MessageBox.Show("Неверные исходные данные! ");
                 else a = 1;
                 return a;
             }
         }
 
-        class Program1
-        {
-            static void Main(string[] args)
-            {
-                //Общение с пользователем начало
-                Console.WriteLine("Гражданин 1 марта открыл счет в банке, вложив A руб.\nЧерез каждый месяц размер вклада увеличивается на 2% от имеющейся суммы.");
-                Console.WriteLine("а) за какой месяц величина ежемесячного увеличения вклада превысит B руб.;\nб) через сколько месяцев размер вклада превысит C руб\n\n");
-                Console.WriteLine("Введите сумму для вклада");
-                double A = int.Parse(Console.ReadLine());
-                double B = int.Parse(Console.ReadLine());
-                double C = int.Parse(Console.ReadLine());
-                Console.WriteLine("\nОТВЕТ:");
-                //Общение с пользователем конец
-                //Начало логики
-                if (Logic.correctinput(A, B, C) == 1)
-                {
-                    var fc = Logic.firstcond(A, B);
-                    var sc = Logic.secondcond(A, C);
-                }
-                else System.Diagnostics.Process.GetCurrentProcess().Kill(); //закрытие программы
-            }
-        }
-
         private void Count_Click(object sender, EventArgs e)
         {
-            double A1 = int.Parse(this.SumA.Text);
-            double B1 = int.Parse(this.SumB.Text);
-            double C1 = int.Parse(this.SumC.Text);
-            if (Logic.correctinput(A1, B1, C1) == 1) 
-            { 
-            Logic.firstcond(A1, B1);
-            Logic.secondcond(A1, C1);
+            double A = double.Parse(this.SumA.Text);
+            double B = double.Parse(this.SumB.Text);
+            double C = double.Parse(this.SumC.Text);
+            if (Logic.correctinput(A, B, C) == 1)
+            {
+                //Logic.firstcond(A, B);
+                //Logic.secondcond(A, C);
+                string result = Logic.firstcond(A, B) + "\n" + Logic.secondcond(A, C);
+                MessageBox.Show(result);
             }
+            else MessageBox.Show("Неверные исходные данные!");
         }
     }
 }
