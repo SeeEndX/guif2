0ebed2a89d4a531734704bd834e5f01b7559508b
Арсений Сендецкий <ars_121229@mail.ru>
2021-10-20 17:53:39 +0800

    Revert "Добавление автотестов"
    This reverts commit e24f0def16eef99453865f24ad80dc9ad1bf42e5.

--- Form1.cs
+++ Form1.cs
@@ -17,50 +17,12 @@ namespace guif2
             InitializeComponent();
             SumA.Text = Properties.Settings.Default.SumA;
             SumB.Text = Properties.Settings.Default.SumB;
+            SumC.Text = Properties.Settings.Default.SumC;
         }
 
-        private void Count_Click(object sender, EventArgs e)
-        {
-            
-            double A = double.Parse(this.SumA.Text);
-            double B = double.Parse(this.SumB.Text);
-            double B1 = double.Parse(this.SumB.Text);
-            if (Logic.correctinput(A, B) == 1)
-            {
-                //string result = Logic.firstcond(A, B) + "\n\n" + Logic.secondcond(A, B1);
-                MessageBox.Show(result);
-            }
-            else MessageBox.Show("Неверные исходные данные!");
-            Properties.Settings.Default.SumA = this.SumA.Text;
-            Properties.Settings.Default.SumB = this.SumB.Text;
-            Properties.Settings.Default.Save();
-        }
-
-        private void Clear_Click(object sender, EventArgs e)
-        {
-            this.SumA.Text = "";
-            this.SumB.Text = "";
-            Properties.Settings.Default.SumA = this.SumA.Text;
-            Properties.Settings.Default.SumB = this.SumB.Text;
-            Properties.Settings.Default.Save();
-        }
-
-
-        private void Task_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Гражданин 1 марта открыл счет в банке, вложив A руб.\n" +
-                "Через каждый месяц размер вклада увеличивается на 2% от имеющейся суммы.\n" +
-                "Определить:\n а) за какой месяц величина ежемесячного увеличения вклада превысит B руб.;\n" +
-                " б) через сколько месяцев размер вклада превысит C руб");
-            Properties.Settings.Default.SumA = this.SumA.Text;
-            Properties.Settings.Default.SumB = this.SumB.Text;
-            Properties.Settings.Default.Save();
-        }
-
-        public string result = "";
         public class Logic
-        { 
-            public static int firstcond(double A, double B)
+            {
+            public static string firstcond(double A, double B)
             {
                 int month1 = 0;
                 double A1 = A;
@@ -73,57 +35,67 @@ namespace guif2
                     else if (A1 - A >= B) i = 1000;
                     if (A2 > B) i = 1000;
                 }
-               // string result1 = "За " + month1 + "й мес. величина ежемесячного увеличения вклада превысит " + B + " руб";
-                return month1;
+                string result1 = "За "+month1+"й мес. величина ежемесячного увеличения вклада превысит "+B+" руб";
+                return result1;
             }
-            public static int secondcond(double A, double B)
+            public static string secondcond(double A, double C)
             {
                 double A2 = A;
                 int month2 = 0;
                 for (int j = 0; j < 1000; j++)
                 {
-                    while (B > A2)
+                    while (C > A2)
                     {
                         A2 = A2 * 1.02;
                         month2 = month2 + 1;
                     }
                 }
-                //string result2 = "Через " + month2 + " мес. размер вклада превысит " + B1 + " руб";
-                return month2;
+                string result2 = "Через " + month2 + " мес. размер вклада превысит " + C + " руб";
+                return result2;
             }
-
-            public static int correctinput(double A, double B)
+            public static int correctinput(double A, double B, double C)
             {
                 int a = 0;
-                if (A <= 0 || B <= 0) MessageBox.Show("Неверные исходные данные! ");
+                if (A <= 0 || B <= 0 || C <= 0) MessageBox.Show("Неверные исходные данные! ");
                 else a = 1;
                 return a;
             }
         }
 
-        private void radioButton1_CheckedChanged(object sender, EventArgs e) //условие б)
+        private void Count_Click(object sender, EventArgs e)
         {
-            RadioButton radioButton = (RadioButton)sender;
             double A = double.Parse(this.SumA.Text);
             double B = double.Parse(this.SumB.Text);
-            if (radioButton.Checked)
+            double C = double.Parse(this.SumC.Text);
+            if (Logic.correctinput(A, B, C) == 1)
             {
-                string result1 = "За " + Logic.firstcond(A,B) + "й мес. величина ежемесячного увеличения вклада превысит " + B + " руб";
-                result = result1;
+                string result = Logic.firstcond(A, B) + "\n\n" + Logic.secondcond(A, C);
+                MessageBox.Show(result);
             }
-            radioButton = (RadioButton)sender;
+            else MessageBox.Show("Неверные исходные данные!");
+            Properties.Settings.Default.SumA = this.SumA.Text;
+            Properties.Settings.Default.SumB = this.SumB.Text;
+            Properties.Settings.Default.SumC = this.SumC.Text;
+            Properties.Settings.Default.Save();
         }
 
-        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        private void Clear_Click(object sender, EventArgs e)
         {
-            RadioButton radioButton = (RadioButton)sender;
-            double A = double.Parse(this.SumA.Text);
-            double B = double.Parse(this.SumB.Text);
-            if (radioButton.Checked)
-            {
-                string result2 = "Через " + Logic.secondcond(A,B) + " мес. размер вклада превысит " + B + " руб";
-                result = result2;
-            }
+            this.SumA.Text = "";
+            this.SumB.Text = "";
+            this.SumC.Text = "";
+        }
+
+        private void Task_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Гражданин 1 марта открыл счет в банке, вложив A руб.\n" +
+                "Через каждый месяц размер вклада увеличивается на 2% от имеющейся суммы.\n" +
+                "Определить:\n а) за какой месяц величина ежемесячного увеличения вклада превысит B руб.;\n" +
+                " б) через сколько месяцев размер вклада превысит C руб");
+            Properties.Settings.Default.SumA = this.SumA.Text;
+            Properties.Settings.Default.SumB = this.SumB.Text;
+            Properties.Settings.Default.SumC = this.SumC.Text;
+            Properties.Settings.Default.Save();
         }
     }
 }
